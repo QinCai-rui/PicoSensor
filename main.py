d022719ce83927637839068f273f9db12abc38cb
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2024-08-13 20:26:55 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from machine import Pin, I2C
 import ssd1306
 import dht
-import utime
-import statistics
+import utime # Apparently you can't use the time library
+
+""" This is a version that works right now but doesn't have all features """
 
 # Initialize I2C for the SSD1306 OLED display
 i2c = I2C(0, scl=Pin(17), sda=Pin(16))
@@ -11,8 +12,6 @@ oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 # Initialize the DHT22 sensor
 sensor = dht.DHT22(Pin(22))
 
-tempHist = []
-humidHist = []
 
 while True:
     try:
@@ -21,14 +20,6 @@ while True:
         temp = sensor.temperature()
         humid = sensor.humidity()
         
-        # Add current temperature and humidity to the history lists
-        tempHist.append(temp)
-        humidHist.append(humid)
-
-        # Calculate average temperature and humidity
-        avg_temp = statistics.mean(tempHist)
-        avg_humid = statistics.mean(humidHist)
-
         # Get the current time
         current_time = utime.localtime()
         time_str = "{:02}:{:02}:{:02}".format(current_time[3], current_time[4], current_time[5])
@@ -40,8 +31,6 @@ while True:
         oled.text("Temp: {:.1f}C".format(temp), 0, 0)
         oled.text("Humid: {:.1f}%".format(humid), 0, 10)
         oled.text("Time: {}".format(time_str), 0, 20)
-        oled.text("Avg. Temp: {:.1f}C".format(avg_temp), 0, 30)
-        oled.text("Avg. Humid: {:.1f}%".format(avg_humid), 0, 40)
         
         # Update the display
         oled.show()
@@ -50,4 +39,7 @@ while True:
         utime.sleep(1)
         
     except OSError as e:
-        print("Oops... Something happened")
+        print('Oops! Something went wrong...')
+    except Exceptions as exc:
+        print('Hmm... Something unexpected happened.')
+        print(exc)
